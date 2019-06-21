7d4195f23d79f06e30d01819b7e080ea3926fc02
fadillaharsa <arsabandung@gmail.com>
2019-06-21 13:26:01 +0700

    Japan OCR

---
+++ Japanese OCR/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Japanese_OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(496, 373);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(488, 347);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(19, 257);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(231, 22);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(268, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 22);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(19, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(463, 245);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Output Mode :";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(268, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 22);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "To Romaji";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(145, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 22);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "To Katakana";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "To Hiragana";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.radioButton3);
+            this.tabPage2.Controls.Add(this.radioButton2);
+            this.tabPage2.Controls.Add(this.radioButton1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.richTextBox3);
+            this.tabPage2.Controls.Add(this.button8);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(488, 347);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(294, 307);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Process";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(425, 278);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Romaji";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(348, 278);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Katakana";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(274, 278);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Hiragana";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Camera Mode :";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(3, 6);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(209, 266);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(388, 307);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 23);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Stop Camera";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(192, 307);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Start Camera";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(218, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 397);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Japanese OCR";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Japanese OCR/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Japanese_OCR
+{
+    public partial class Form1 : Form
+    {
+        VideoCaptureDevice frame;
+        FilterInfoCollection Devices;
+        OpenFileDialog openfile = new OpenFileDialog();
+        String inputText;
+        enum Mode
+        {
+            Hiragana,
+            Katakana,
+            Romaji,
+        }
+
+        private List<string> Database = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            GetDatabase();
+            tabPage1.Text = "Photos";
+            tabPage2.Text = "Camera";
+        }
+
+        private void GetDatabase()
+        {
+            using (System.IO.StreamReader sr = new System.IO.StreamReader("Database.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string splitMe = sr.ReadLine();
+                    Database.Add(splitMe);
+                }
+            }
+        }
+
+        private string Convert(string text, Mode convertMode)
+        {
+            text = text.ToLower();
+
+            string roma = string.Empty;
+            string hira = string.Empty;
+            string kata = string.Empty;
+
+            foreach (string row in Database)
+            {
+                var split = row.Split('@');
+                roma = split[0];
+                hira = split[1];
+                kata = split[2];
+
+                switch (convertMode)
+                {
+                    case Mode.Romaji:
+                        text = text.Replace(hira, roma);
+                        text = text.Replace(kata, roma.ToUpper());
+                        break;
+                    case Mode.Hiragana:
+                        text = text.Replace(roma, hira);
+                        text = text.Replace(kata, hira);
+                        break;
+                    case Mode.Katakana:
+                        text = text.Replace(roma, kata);
+                        text = text.Replace(hira, kata);
+                        break;
+                }
+            }
+            return text;
+        }
+        //Process from photo setting
+        private String process()
+        {
+            Bitmap img = new Bitmap(richTextBox2.Text);
+            TesseractEngine ocr = new TesseractEngine("./tessdata", "jpn", EngineMode.Default);
+            Page page = ocr.Process(img, PageSegMode.Auto);
+            inputText = page.GetText();
+            return inputText;
+        }
+        //Camera Setting
+        private void Start_Cam()
+        {
+            try
+            {
+                Devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                frame = new VideoCaptureDevice(Devices[0].MonikerString);
+                frame.NewFrame += new AForge.Video.NewFrameEventHandler(NewFrame_event);
+                frame.Start();
+                button6.Enabled = true;
+                button8.Enabled = true;
+            }
+            catch(Exception)
+            {
+                richTextBox3.Text = "No camera device applied!";
+            }
+        }
+
+        private void NewFrame_event(object send, NewFrameEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = (Image)e.Frame.Clone();
+            }
+            catch (Exception)
+            {
+                richTextBox3.Text = "No camera device applied!";
+            }
+        }
+        //Button setting
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            String inputText = process();
+            richTextBox1.Text = Convert(inputText, Mode.Hiragana);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String inputText = process();
+            richTextBox1.Text = Convert(inputText, Mode.Katakana);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String inputText = process();
+            richTextBox1.Text = Convert(inputText, Mode.Romaji);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox2.Text = openfile.FileName;
+            }
+            else
+            {
+                inputText = "";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Start_Cam();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            if(frame!=null)
+                frame.Stop();
+            button8.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(frame!=null)
+                frame.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                richTextBox3.Text = "No camera device applied!";
+            else
+            {
+                Bitmap img = new Bitmap(pictureBox1.Image);
+                TesseractEngine ocr = new TesseractEngine("./tessdata", "jpn", EngineMode.Default);
+                Page page = ocr.Process(img, PageSegMode.Auto);
+                String inputText = page.GetText();
+                if (radioButton1.Checked)
+                {
+                    richTextBox3.Text = Convert(inputText, Mode.Hiragana);
+                }
+                else if (radioButton2.Checked)
+                {
+                    richTextBox3.Text = Convert(inputText, Mode.Katakana);
+                }
+                else if (radioButton3.Checked)
+                {
+                    richTextBox3.Text = Convert(inputText, Mode.Romaji);
+                }
+            }
+        }
+    }
+}
